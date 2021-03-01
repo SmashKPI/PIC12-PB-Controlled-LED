02764e76ee75e37ee152140817f963c903386438
Dmytro Tsebrii <67375532+SmashKPI@users.noreply.github.com>
2021-03-01 02:12:31 -0500

    Update gpioTest.asm

--- gpioTest.asm
+++ gpioTest.asm
@@ -3,7 +3,7 @@
 ;	Author:	DTsebrii
 ;	Date:	02/27/2021
 ;	Description:	Program to turn on and the LED on PIC12F1822 if
-;			user presses a pushbotton
+;					user presses a pushbotton
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
 ;
@@ -30,7 +30,7 @@ FreqVal		EQU	b'01110000'	; 8MHz
 InitPort	EQU	b'00000000'	; PORTA all Voltage are low
 TRISconf	EQU	b'11011111'	; All inputs except RA5 
 AllDigit	EQU	b'00000000'	; Variable to conf ANSELA
-LED		EQU	b'00000101'	; Representation of 5-th bit
+LED			EQU	b'00000101'	; Representation of 5-th bit
 #define	PUSHB	PORTA, 4 ; RA4 is pushbutton 
 
 	ORG	0x00
@@ -39,24 +39,17 @@ MainRoutine
 	CALL sysInit	; Function to initialize the system
 loop1
 	BANKSEL	PORTA 
-	BTFSS	PUSHB	; Check if pb is pressed
-	CALL	blowLED
-	BTFSC	PUSHB 
-	CALL	unblowLED
+	BTFSC	PUSHB	; Check if pb is pressed
+	GOTO	ledOff	; turn off LED 
+	BANKSEL	LATA	; set led otherwise
+	BSF		LATA, LED
+	GOTO	loop1
+	BTFSS	PUSHB 
+ledOff
+	BANKSEL LATA
+	BCF		LATA, LED
 	GOTO	loop1
 
-
-blowLED
-	BANKSEL	LATA
-	BSF	LATA, LED
-	
-	RETURN
-
-unblowLED
-	BANKSEL	LATA
-	BCF	LATA, LED
-	
-	RETURN
 
 
 ;;;; sysConfig ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -112,23 +105,3 @@ portConfig
 
 
 	END
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
